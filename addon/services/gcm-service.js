72e686ef50e16d30cee1040b3f746cf3c7bcc564
Offir Golan <offirgolan@yahoo-inc.com>
2015-04-28 17:47:36 -0700

    convert to promise based service

--- addon/services/gcm-service.js
+++ addon/services/gcm-service.js
@@ -12,125 +12,142 @@ const {
 } = Ember;
 
 export default Ember.Service.extend(Ember.TargetActionSupport, {
+    initializer: null,
 
-    // Actions
+    init: function() {
+        this._registerServiceWorker();
+        this._super.apply(this, arguments);
+    },
 
-    _registerServiceWorker: on('init', function() {
-        if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('service-worker.js').then(this._initializeState.bind(this));
+    reInitialize: function() {
+        this._registerServiceWorker();
+        return get(this, 'initializer');
+    },
+
+    _serviceWorkerAvailable: computed(function() {
+        return 'serviceWorker' in navigator;
+    }).readOnly(),
+
+    _registerServiceWorker: function() {
+        var promise = null;
+        if (get(this, '_serviceWorkerAvailable')) {
+            promise = navigator.serviceWorker.register('service-worker.js').then(this._initializeState.bind(this));
         } else {
-            console.warn('Service workers aren\'t supported in this browser.');
+            promise = new Promise(function(resolve, reject) {
+                reject('Service workers aren\'t supported in this browser.');
+            });
         }
-    }),
 
-    // _unregister: on('willDestroy', function() {
-    //     this.unsubscribe();
-    // }),
+        set(this, 'initializer', promise);
+    },
+
 
     _initializeState: function() {
-        var self = this;
-
         // Are Notifications supported in the service worker?
         if (!('showNotification' in ServiceWorkerRegistration.prototype)) {
-            console.warn('Notifications aren\'t supported.');
-            return;
+            return new Promise(function(resolve, reject) {
+                reject('Notifications aren\'t supported.');
+            });
         }
 
         // Check the current Notification permission.
         // If its denied, it's a permanent block until the
         // user changes the permission
         if (Notification.permission === 'denied') {
-            console.warn('The user has blocked notifications.');
-            return;
+            return new Promise(function(resolve, reject) {
+                reject('The user has blocked notifications.');
+            });
         }
 
         // Check if push messaging is supported
         if (!('PushManager' in window)) {
-            console.warn('Push messaging isn\'t supported.');
-            return;
+            return new Promise(function(resolve, reject) {
+                reject('Push messaging isn\'t supported.');
+            });
         }
 
-
         // We need the service worker registration to check for a subscription
-        navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
+        return navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
             // Do we already have a push message subscription?
-            serviceWorkerRegistration.pushManager.getSubscription()
-                .then(function(subscription) {
-                    // TODO: send action
-                    // self.send('on-initilize-success', subscription);
-
-                })
-                .catch(function(err) {
-                    // TODO: send action
-                    console.warn('Error during getSubscription()', err);
-                });
+            return serviceWorkerRegistration.pushManager.getSubscription();
         });
     },
 
     subscribe: function() {
-        navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
-            serviceWorkerRegistration.pushManager.subscribe()
-                .then(function(subscription) {
-                    // The subscription was successful
+        if (!get(this, '_serviceWorkerAvailable')) {
+            return new Promise(function(resolve, reject) {
+                reject('Service workers aren\'t supported in this browser.');
+            });
+        }
 
-
-                    // TODO: Send the subscription.subscriptionId and
-                    // subscription.endpoint to your server
-                    // and save it to send a push message at a later date
-                    //return sendSubscriptionToServer(subscription);
-                    //
-                    // TODO: send action
-                    return;
-                })
-                .catch(function(e) {
-                    if (Notification.permission === 'denied') {
-                        // The user denied the notification permission which
-                        // means we failed to subscribe and the user will need
-                        // to manually change the notification permission to
-                        // subscribe to push messages
-                        console.warn('Permission for Notifications was denied');
-                    } else {
-                        // A problem occurred with the subscription; common reasons
-                        // include network errors, and lacking gcm_sender_id and/or
-                        // gcm_user_visible_only in the manifest.
-                        console.error('Unable to subscribe to push.', e);
-                    }
-                });
+        return Ember.get(this, 'initializer').then(function() {
+            return navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
+                return serviceWorkerRegistration.pushManager.subscribe();
+            });
         });
     },
 
     unsubscribe: function() {
-        navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
-            // To unsubscribe from push messaging, you need get the
-            // subscription object, which you can call unsubscribe() on.
-            serviceWorkerRegistration.pushManager.getSubscription().then(
-                function(pushSubscription) {
-                    // Check we have a subscription to unsubscribe
-                    if (!pushSubscription) {
-                        // No subscription object, so set the state
-                        // to allow the user to subscribe to push
-                        return;
-                    }
-
-                    var subscriptionId = pushSubscription.subscriptionId;
-                    // TODO: Make a request to your server to remove
-                    // the subscriptionId from your data store so you
-                    // don't attempt to send them push messages anymore
-
-                    // We have a subscription, so call unsubscribe on it
-                    pushSubscription.unsubscribe().then(function(successful) {
-                        //Do something
-                    }).catch(function(e) {
-                        // We failed to unsubscribe, this can lead to
-                        // an unusual state, so may be best to remove
-                        // the users data from your data store and
-                        // inform the user that you have done so
-
-                        console.log('Unsubscription error: ', e);
-                    });
-                }).catch(function(e) {
-                console.error('Error thrown while unsubscribing from push messaging.', e);
+        if (!get(this, '_serviceWorkerAvailable')) {
+            return new Promise(function(resolve, reject) {
+                reject('Service workers aren\'t supported in this browser.');
             });
+        }
+
+        return Ember.get(this, 'initializer').then(function() {
+            return navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
+                // To unsubscribe from push messaging, you need get the
+                // subscription object, which you can call unsubscribe() on.
+                return serviceWorkerRegistration.pushManager.getSubscription()
+                    .then(function(subscription) {
+                        // Check we have a subscription to unsubscribe
+                        if (!subscription) {
+                            // No subscription object, so set the state
+                            // to allow the user to subscribe to push
+                            return new Promise(function(resolve, reject) {
+                                reject('No subscription found.');
+                            });
+                        }
+
+                        // TODO: Make a request to your server to remove
+                        // the subscriptionId from your data store so you
+                        // don't attempt to send them push messages anymore
+
+                        // We have a subscription, so call unsubscribe on it
+                        return subscription.unsubscribe();
+                        // .catch(function(e) {
+                        //     // We failed to unsubscribe, this can lead to
+                        //     // an unusual state, so may be best to remove
+                        //     // the users data from your data store and
+                        //     // inform the user that you have done so
+
+                        //     console.log('Unsubscription error: ', e);
+                        // });
+                    });
+            });
+        });
+    },
+
+    requestPermission: function() {
+        if (Notification) {
+            return new Promise(function(resolve, reject) {
+                Notification.requestPermission(function(status) {
+                    if (status !== 'granted') {
+                        reject(status);
+                    }
+                    resolve(status);
+                });
+            });
+        } else {
+            return new Promise(function(resolve, reject) {
+                reject('Notifications aren\'t supported.');
+            });
+        }
+    },
+
+    push: function(title = '', options = {}) {
+        return this.requestPermission().then(function() {
+            return new Notification(title, options);
         });
     }
 
